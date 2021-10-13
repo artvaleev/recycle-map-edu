9ea010a12b7d92807a11bf89c82e9ccaf2b0f3d0
Artur Valeev <artur.valeev@akvelon.com>
2021-10-13 19:09:50 +0300

    = maps in progress

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,62 @@
 <template>
-  <div id="app">
-    <h1>Hello</h1>
-    
-    <router-view />
+  <div id="app">    
+    <div id="map">
+      <yandex-map :settings="settings" :coords="coords" :zoom="12" :controls="['zoomControl' ,'trafficControl']">
+        <ymap-marker 
+          :coords="coords" 
+          marker-id="123" 
+          hint-content="some hint"
+          @click="onMarkerClick"
+          :balloon="{ text: 'hi'}"
+        />
+      </yandex-map>
+    </div>
   </div>
 </template>
 
 <script>
+import { yandexMap, ymapMarker } from 'vue-yandex-maps';
+import { settings } from './maps_settings';
 export default {
   name: 'App',
+  data() {
+    return { settings, coords: [
+      55.779563, 49.132241
+    ] };
+  },
+  mounted() {
+    console.log(process.env.VUE_APP_YMAPS_API_KEY);
+    console.log(process.env);
+  },
+  components: { yandexMap, ymapMarker },
+  methods: {
+    onMarkerClick(e) {
+      console.log(e.get('target'));
+    },
+  },
 }
 </script>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+#map {
+  width: 100%;
+  height: 100vh;
+  background: grey;
+}
+
+#map > * {
+  height: inherit;
 }
 </style>
---
+++ src/maps_settings.js
@@ -0,0 +1,6 @@
+export const settings = {
+  apiKey: '74f907ca-a045-429b-adbc-82aa2aebd671',
+  lang: 'ru_RU',
+  coordorder: 'latlong',
+  version: '2.1'
+};
